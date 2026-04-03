51eb4d634d7c5c24b39be6247b035bb1b3263b86
markgloverswaybtinternetcom <106952544+markgloverswaybtinternetcom@users.noreply.github.com>
2026-04-03 18:28:32 +0100

    work with standard batdetect2 install

--- bd2Link.py
+++
@@ -1,8 +0,0 @@
-import os
-
-bd2_link = os.path.join(os.getcwd(), "batdetect2")
-if not os.path.exists(bd2_link):
-    print("No link {bd2_link=}")
-    # Create symbolic link to BatDetect2 code
-    bd2 = os.path.join(os.getcwd(), "libs", "batdetect2", "batdetect2")
-    os.symlink(bd2, bd2_link, target_is_directory=True)        
